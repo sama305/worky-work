e90e9b30fcab4d44c3a3abd834eee89043b3efd6
sam-morgan <andersonhenderson45@gmail.com>
2023-07-18 20:36:48 -0400

    overhauled split tile

--- worky-app/src/components/SplitTile.vue
+++ worky-app/src/components/SplitTile.vue
@@ -1,12 +1,19 @@
 <template>
-    <p style="margin: 0; margin-bottom: 0"><b><i>{{ split.name }}</i> ({{ split.days.length }} days)</b></p>
+    <p v-if="header" style="margin: 0; margin-bottom: 0">{{ split.name }} ({{ split.days.length }} days)</p>
     <table style="margin-bottom: 5px">
-        <tr v-for="day in split.days">
-            <td>{{ day.name }}</td>
-            <td v-for="e in day.exercises">
-                <RouterLink :to="`/exercises/${e.id}`" class="rawLink">
-                    {{ e.name }}
-                </RouterLink>
+        <tr>
+            <th v-for="day in split.days">{{ day.name }}</th>
+        </tr>
+        <tr v-for="i in getMaxExercisesInSplit(split)">
+            <td v-for="d in split.days" class="monoInfoCell" :title="d.exercises[i-1] ? d.exercises[i-1].name : ''">
+                <template v-if="d.exercises[i-1]">
+                        <RouterLink :to="`/exercises/${d.exercises[i-1].id}`" class="rawLink exerciseLink">
+                            {{ d.exercises[i-1].name }}
+                        </RouterLink>
+                </template>
+                <template v-else>
+                        ~
+                </template>
             </td>
         </tr>
     </table>
@@ -20,6 +27,24 @@ export default {
         split: {
             type: Split,
             required: true
+        },
+        header: {
+            type: Boolean,
+            required: false,
+            default: true
+        }
+    },
+    methods: {
+        getMaxExercisesInSplit(s: Split): number {
+            let max: number = s.days[0].exercises.length
+            if (s.days.length > 1) {
+                s.days.forEach(d => {
+                    if (d.exercises.length > max) {
+                        max = d.exercises.length
+                    }
+                })
+            }
+            return max
         }
     }
 }
